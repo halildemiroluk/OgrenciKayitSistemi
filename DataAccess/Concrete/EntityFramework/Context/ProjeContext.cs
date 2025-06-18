b9e21f0b6df564f41db9c06b6552ab0ca7d3a988
Halil İbrahim Demiroluk <demiroluk2001@gmail.com>
2025-06-18 22:16:41 +0300

    Temiz proje yüklendi

---
+++ DataAccess/Concrete/EntityFramework/Context/ProjeContext.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework.Context
+{
+    public class ProjeContext:DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) // ovveride on yaz gerisi gelir
+        {
+            optionsBuilder.UseSqlServer(
+    "Server=LAPTOP-K7TJ6CRH\\SQLEXPRESS;Database=OgrenciKayitSitemiMVC;Integrated Security=True;TrustServerCertificate=True");
+
+           
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
